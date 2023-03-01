0143a0fc1d14340483922de26ad3749751beb2ae
affanwarunkar <affanwarunkar69@gmail.com>
2023-03-01 08:57:26 +0000

    updating file

---
+++ Additionofele.java
@@ -0,0 +1,13 @@
+public class Additionofele {
+    public static void main(String args[]){
+        int a[]= new int[]{1,2,3,4,5};
+        int sum=0;
+        for(int i=0;i<5;i++){
+            sum=sum+a[i];
+        }
+        System.out.println("addition of all elements"+sum);
+
+    }
+    
+}
+//faisal hai gandu
